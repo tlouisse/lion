18cbc50b14156a0938ff27b0744e50a7f4a20154
Mikhail Bashkirov <mikhail.bashkirov@ing.com>
2019-05-09 12:08:50 +0200

    fix(calendar): isSameDate should not fail on non-dates

--- packages/calendar/src/utils/isSameDate.js
+++ packages/calendar/src/utils/isSameDate.js
@@ -8,6 +8,8 @@
  */
 export function isSameDate(day1, day2) {
   return (
+    day1 instanceof Date &&
+    day2 instanceof Date &&
     day1.getDate() === day2.getDate() &&
     day1.getMonth() === day2.getMonth() &&
     day1.getFullYear() === day2.getFullYear()
